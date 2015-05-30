84a3180934b0cfbf6dd2fbb1415b7ca363eaae8e
chen2aaron <chan4aaron@163.com>
2015-05-30 22:01:05 +0800

    JSON-kernel_panic and music163

---
+++ Spider/JSON-kernel_panic.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+# -*- coding:utf-8 -*-
+# Created by xixijun
+# Date: 15-5-30
+# Blog: morningchen.com
+
+
+import requests
+import json
+import sys
+reload(sys)
+sys.setdefaultencoding('utf-8')
+r = requests.get("http://www.lizhi.fm/api/radio_audios?s=0&l=20&band=474221")
+content = r.text
+result = json.loads(content)
+for i in result:
+    print i['url']
