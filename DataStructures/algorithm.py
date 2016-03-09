08ae2d7110374fff285281b8fa3209d250ee7a48
chan <chan4aaron@163.com>
2016-03-09 21:31:18 +0800

    optimize

--- DataStructures/algorithm.py
+++ DataStructures/algorithm.py
@@ -30,10 +30,18 @@ t4 =Timer('test4', 'from __main__ import test4')
 print('list range:   ', t4.timeit(number=10000000), 'milliseconds')
 
 x = list(range(2000000))
-pop_zero = Timer("x.pop(0)", "from __main__ import x")
-print("pop_zero ", pop_zero.timeit(number=1000), "milliseconds")
-pop_end = Timer("x.pop()", "from __main__ import x")
-print("pop_end ", pop_end.timeit(number=1000), "milliseconds")
+pop_front = Timer("x.pop(0)", "from __main__ import x")
+print("pop_front     ", pop_front.timeit(number=1000), "milliseconds")
+y = list(range(2000000))
+pop_end = Timer("y.pop()", "from __main__ import y")
+print("pop_end       ", pop_end.timeit(number=1000), "milliseconds")
+
+m = list(range(2000000))
+insert_front = Timer("m.insert(0, 100)", "from __main__ import m")
+print("insert_front  ", insert_front.timeit(number=1000))
+n = list(range(2000000))
+insert_end = Timer("n.append(100)", "from __main__ import n")
+print("insert_end    ", insert_end.timeit(number=1000))
 
 import timeit
 import random
--- PlaySQL/test_autobid.py
+++ PlaySQL/test_autobid.py
@@ -1,5 +1,6 @@
 import json
 from decimal import Decimal
+from pprint import pprint
 from Utils import torndb
 
 
@@ -15,25 +16,36 @@ def db():
         read_timeout=10,
         write_timeout=15,
     )
-borrowing = db().get('select * from borrowings where id=%s', 544)
+borrowing = db().get('select * from borrowings where id=%s', 555)
 coupon = db().query('select c.id, c.type, c.value_a, c.value_b, c.limits from coupons c '
                   ' where c.user_id=%s and c.status=0 and c.expire_at > NOW() '
                   ' order by c.expire_at', 83028)
 valid_coupon = []
-coupon_interest = 0
 money_min = Decimal('100.00')
-money_max = Decimal('1100.00')
-
+money_max = Decimal('1000.00')
 duration = 'xd' if borrowing.duration_type == 0 else '%sm' % borrowing.duration
 for c in coupon:
+    coupon_interest = Decimal('0.00')
     if c.limits and duration not in json.loads(c.limits).get('duration', ''):
         continue
     if c.type == 0 and c.value_a <= max(money_min, money_max):
         coupon_interest = c.value_b
     if c.type == 1:
-        coupon_interest = max(money_min, money_max) * c.value_a
-    valid_coupon.append((c.id, coupon_interest))
-print(valid_coupon)
-coupon_select = max(valid_coupon, key=lambda c: c[1])
-print(coupon_select)
-print(borrowing)
+        if borrowing.duration_type == 0:
+            coupon_interest = round(max(money_min, money_max) * c.value_a / 365 * borrowing.duration, 2)
+        else:
+            bonus_interest_limit = json.loads(c.limits).get('bonus_interest_limit', '')
+            if bonus_interest_limit:
+                coupon_interest = round(max(money_min, money_max) * c.value_a / 12 * min(bonus_interest_limit, borrowing.duration), 2)
+            else:
+                coupon_interest = round(max(money_min, money_max) * c.value_a / 12 * borrowing.duration, 2)
+    valid_coupon.append((c.id, coupon_interest, c.value_b or Decimal('0.00')))
+if valid_coupon:
+    # 选择收益最大的优惠券
+    selected_coupon = max(valid_coupon, key=lambda c: c[1])
+    coupon_id = selected_coupon[0]
+    money_max = max(money_min, money_max) + selected_coupon[2]
+    print(selected_coupon)
+
+pprint(valid_coupon)
+print(max(money_min, money_max))
