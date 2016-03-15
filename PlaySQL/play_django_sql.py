d2a2e3964508d9b3c30de8cbd9e6d25b5c6523b1
chan <chan4aaron@163.com>
2016-03-15 23:42:09 +0800

    update bank_limit

--- PlaySQL/play_django_sql.py
+++ PlaySQL/play_django_sql.py
@@ -16,22 +16,23 @@ def db():
     return db
 
 
-bank_limits_data ={
-    'BOC':    { 'name': '中国银行', 'quick': 10000, 'binding_first': 10000, 'binding': 10000, 'one_day': 10000   },
-    'ABC':    { 'name': '农业银行', 'quick': 20000, 'binding_first': 50000, 'binding': 50000, 'one_day': 100000  },
-    'CCB':    { 'name': '建设银行', 'quick': 50000, 'binding_first': 50000, 'binding': 50000, 'one_day': 1000000 },
-    'ICBC':   { 'name': '工商银行', 'quick': 10000, 'binding_first': 50000, 'binding': 50000, 'one_day': 50000   },
-    'CMB':    { 'name': '招商银行', 'quick': 5000,  'binding_first': 5000,  'binding': 50000, 'one_day': 200000  },
-    'CITIC':  { 'name': '中信银行', 'quick': 50000, 'binding_first': 50000, 'binding': 50000, 'one_day': 1000000 },
-    'CMBC':   { 'name': '民生银行', 'quick': 5000,  'binding_first': 5000,  'binding': 50000, 'one_day': 1000000 },
-    'GDB':    { 'name': '广发银行', 'quick': 5000,  'binding_first': 5000,  'binding': 50000, 'one_day': 1000000 },
-    'CIB':    { 'name': '兴业银行', 'quick': 20000, 'binding_first': 50000, 'binding': 50000, 'one_day': 500000  },
-    'CEB':    { 'name': '光大银行', 'quick': 50000, 'binding_first': 50000, 'binding': 50000, 'one_day': 1000000 },
-    'BOS':    { 'name': '上海银行', 'quick': 5000,  'binding_first': 5000,  'binding': 5000,  'one_day': 50000   },
-    'PSBC':   { 'name': '邮储银行', 'quick': 0,     'binding_first': 5000,  'binding': 5000,  'one_day': 5000    },
-    'HXB':    { 'name': '华夏银行', 'quick': 5000,  'binding_first': 5000,  'binding': 5000,  'one_day': 50000   },
-    'SZPAB':  { 'name': '平安银行', 'quick': 5000,  'binding_first': 5000,  'binding': 50000, 'one_day': 1000000 },
-    'SPDB':   { 'name': '浦发银行', 'quick': 5000,  'binding_first': 5000,  'binding': 5000,  'one_day': 10000   }
+bank_limits_data = {
+    'COMM':   {'name': '交通银行', 'quick': 20000, 'binding_first': 20000, 'binding': 50000, 'one_day': 500000},
+    'BOC':    {'name': '中国银行', 'quick': 10000, 'binding_first': 10000, 'binding': 10000, 'one_day': 10000},
+    'ABC':    {'name': '农业银行', 'quick': 5000,  'binding_first': 20000, 'binding': 20000, 'one_day': 100000},
+    'CCB':    {'name': '建设银行', 'quick': 50000, 'binding_first': 50000, 'binding': 50000, 'one_day': 500000},
+    'ICBC':   {'name': '工商银行', 'quick': 50000, 'binding_first': 50000, 'binding': 50000, 'one_day': 50000},
+    'CITIC':  {'name': '中信银行', 'quick': 50000, 'binding_first': 50000, 'binding': 50000, 'one_day': 500000},
+    'CMBC':   {'name': '民生银行', 'quick': 5000,  'binding_first': 50000, 'binding': 50000, 'one_day': 1000000},
+    'CIB':    {'name': '兴业银行', 'quick': 50000, 'binding_first': 50000, 'binding': 50000, 'one_day': 50000},
+    'CEB':    {'name': '光大银行', 'quick': 50000, 'binding_first': 50000, 'binding': 50000, 'one_day': 500000},
+    'BOS':    {'name': '上海银行', 'quick': 5000,  'binding_first': 5000,  'binding': 5000,  'one_day': 50000},
+    'PSBC':   {'name': '邮储银行', 'quick': 0,     'binding_first': 5000,  'binding': 5000,  'one_day': 5000},
+    'SZPAB':  {'name': '平安银行', 'quick': 5000,  'binding_first': 5000,  'binding': 5000,  'one_day': 5000},
+    'SPDB':   {'name': '浦发银行', 'quick': 5000,  'binding_first': 20000, 'binding': 20000,  'one_day': 50000},
+    'CMB':    {'name': '招商银行', 'quick': 5000,  'binding_first': 50000, 'binding': 50000, 'one_day': 300000},
+    'HXB':    {'name': '华夏银行', 'quick': 5000,  'binding_first': 5000,  'binding': 5000,  'one_day': 50000},
+    'GDB':    {'name': '广发银行', 'quick': 5000,  'binding_first': 5000,  'binding': 50000, 'one_day': 1000000}
 }
 
 for k, v in bank_limits_data.items():
