c85c4b98985f721439a1a95d80c4417c2cde90cd
chan <chan4aaron@163.com>
2016-05-03 00:28:02 +0800

    Update: chapter8

---
+++ PythonCookbookPractise/chapter8/define_interface_or_abstract_base_class.py
@@ -0,0 +1,23 @@
+# 8.12. Defining an Interface or Abstract Base Class
+
+
+from abc import ABCMeta, abstractmethod
+
+class IStream(metaclass=ABCMeta):
+    @abstractmethod
+    def read(self, maxbytes=-1):
+        pass
+
+    @abstractmethod
+    def write(self, data):
+        pass
+
+# 抽象类的一个特点是它不能直接被实例化
+# a = IStream()
+
+class SocketStream(IStream):
+    def read(self, maxbytes=-1):
+        pass
+
+    def write(self, data):
+        pass
---
+++ PythonCookbookPractise/chapter8/simplify_initialization_of_data_structure.py
@@ -0,0 +1,69 @@
+# 8.11. Simplifying the Initialization of Data Structures
+import math
+
+
+class Structure1(object):
+    _fields = []
+
+    def __init__(self, *args):
+        if len(args) != len(self._fields):
+            raise TypeError('Expected {} arguments'.format(len(self._fields)))
+
+        for name, value in zip(self._fields, args):
+            setattr(self, name, value)
+
+
+
+# Example class definitions
+class Stock(Structure1):
+    _fields = ['name', 'shares', 'price']
+
+class Point(Structure1):
+    _fields = ['x', 'y']
+
+class Circle(Structure1):
+    _fields = ['radius']
+
+    def area(self):
+        return math.pi * self.radius ** 2
+
+
+class Structure2(object):
+    _fields = []
+
+    def __init__(self, *args, **kwargs):
+        if len(args) > len(self._fields):
+            raise TypeError('Expected {} arguments'.format(len(self._fields)))
+
+        # Set all of the positional arguments
+        for name, value in zip(self._fields, args):
+            setattr(self, name, value)
+
+        # Set the remaining keyword arguments
+        for name in self._fields[len(args):]:
+            setattr(self, name, kwargs.pop(name))
+
+        # Check for any remaining unknown arguments
+        if kwargs:
+            raise TypeError('Invalid argument(s): {}'.format(','.join(kwargs)))
+
+
+class Structure3:
+    # Class variable that specifies expected fields
+    _fields = []
+
+    def __init__(self, *args, **kwargs):
+        if len(args) != len(self._fields):
+            raise TypeError('Expected {} arguments'.format(len(self._fields)))
+
+        # Set the arguments
+        for name, value in zip(self._fields, args):
+            setattr(self, name, value)
+
+        # Set the additional arguments (if any)
+        extra_args = kwargs.keys() - self._fields
+        for name in extra_args:
+            setattr(self, name, kwargs.pop(name))
+
+        if kwargs:
+            raise TypeError('Duplicate values for {}'.format(','.join(kwargs)))
